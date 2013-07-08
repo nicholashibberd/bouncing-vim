cc1b33479ae05d90d2435401cded9dd067c89d91
Luca Belmondo <blmlcu@gmail.com>
2013-07-08 19:55:56 +0100

    Fix and rename function to select the plugin name

--- util-functions.sh
+++ util-functions.sh
@@ -2,27 +2,26 @@
 
 set -e
 
-get_basename () {
-  echo "$GITHUB" | sed -r 's|.+\/(.+)\.git$|\1|g'
+github_basename () {
+  echo "$1" | sed -r 's|.+\/(.+)$|\1|g'
 }
 
 clone_to_bundle_dir () {
   local GITHUB=$1
   local BASENAME=$2
   local DIR=$3
+  echo
+
   if [[ ! -d "$HOME/.vim/bundle/$BASENAME" ]]; then
-    echo
     git clone "git@github.com:$GITHUB" "$HOME/.vim/bundle/$DIR"
   else
-    echo 
     echo "$GITHUB already installed, skipping"
   fi
-
 }
 
 clone_temp_plugin () {
   local GITHUB=$1
-  local BASENAME=$(get_basename $GITHUB)
+  local BASENAME=$(github_basename $GITHUB)
   local DIR="vim-luke-tmp-$BASENAME"
 
   clone_to_bundle_dir $GITHUB $BASENAME $DIR
@@ -30,7 +29,7 @@ clone_temp_plugin () {
 
 clone_plugin () {
   local GITHUB=$1
-  local BASENAME=$(get_basename $GITHUB)
+  local BASENAME=$(github_basename $GITHUB)
   local DIR="$BASENAME"
 
   clone_to_bundle_dir $GITHUB $BASENAME $DIR
