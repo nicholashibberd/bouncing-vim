7a4d18403a5f4ff25ce9bffe8861034b180d642c
Luca <blmlcu@gmail.com>
2014-02-21 06:26:02 +0000

    Update tmux compile script: bump version to 1.9 and tidy up

--- tmux-scripts/1-tmux-install-from-source.sh
+++ tmux-scripts/1-tmux-install-from-source.sh
@@ -2,56 +2,80 @@
 
 set -e
 
-TMUX_VERSION=1.8
-TMUX_SOURCE_DIR="${HOME}/Downloads/tmux-${TMUX_VERSION}"
-tmux_download_url="http://downloads.sourceforge.net/project/tmux/tmux/tmux-${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz"
+TMUX_VERSION=1.9
+TMUX_SOURCE_TARBALL="${HOME}/Downloads/tmux-${TMUX_VERSION}.tar.gz"
+TMUX_SOURCE_DIR="tmux-${TMUX_VERSION}"
+TMUX_SOURCE_PATH="${HOME}/Downloads/tmux-${TMUX_VERSION}"
+TMUX_DOWNLOAD_URL="http://downloads.sourceforge.net/project/tmux/tmux/tmux-${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz"
 
 echo
 echo "--- Install tmux ${TMUX_VERSION} from source ---"
 echo
 
+# ====================================
+# === Dependencies and environment ===
+# ====================================
+
 echo "Ensure dependencies for compiling tmux"
 sudo apt-get update
-sudo apt-get -y install curl build-essential libncurses5-dev libevent-dev
+sudo apt-get -y install curl build-essential libncurses-dev libevent-dev
 
 mkdir -v -p "${HOME}/Downloads"
 
 echo "Ensure to remove tmux source dir"
-rm -rf $TMUX_SOURCE_DIR
+rm -rf $TMUX_SOURCE_PATH
+
+# ================
+# === Download ===
+# ================
 
 if [[ -f "${HOME}/Downloads/tmux-${TMUX_VERSION}.tar.gz" ]]; then
   echo "tmux-${TMUX_VERSION}.tar.gz already downloaded"
 else
   echo "Download tmux ${TMUX_VERSION}"
-  cd "${HOME}/Downloads/" && { curl -O -L $tmux_download_url; cd  -; }
+  curl -o $TMUX_SOURCE_TARBALL -L $TMUX_DOWNLOAD_URL
 fi
 
 echo "Extract tmux source archive"
-tar xfz "${HOME}/Downloads/tmux-${TMUX_VERSION}.tar.gz" -C "${HOME}/Downloads/"
+tar xfz $TMUX_SOURCE_TARBALL -C "${HOME}/Downloads/"
+
+# ===========================
+# === Compile and install ===
+# ===========================
 
 echo "run ./configure"
-cd $TMUX_SOURCE_DIR
+cd $TMUX_SOURCE_PATH
 ./configure
 cd -
 
 echo "run make"
-make -C $TMUX_SOURCE_DIR
+make -C $TMUX_SOURCE_PATH
 
-sudo make install -C $TMUX_SOURCE_DIR
+sudo make install -C $TMUX_SOURCE_PATH
 echo
 echo "Installed tmux version $(tmux -V) at $(which tmux)"
 
 cd "$HOME/Downloads"
 
-archive_filename="tmux-${TMUX_VERSION}-compiled-$(date -d "today" +"%Y-%m-%d_%H-%M-%S").tar.gz"
-echo "Archive source to ${archive_filename} for uninstall."
-cd ~/Downloads && { tar cfz $archive_filename "tmux-${TMUX_VERSION}"; cd -; }
+# ============================
+# === Archive and clean up ===
+# ============================
+
+ARCHIVE_FILENAME="tmux-${TMUX_VERSION}-compiled-$(date -d "today" +"%Y-%m-%d_%H-%M-%S").tar.gz"
+echo "Archive source to ${ARCHIVE_FILENAME} for uninstall."
+tar cfz "${HOME}/Downloads/${ARCHIVE_FILENAME}" -C "${HOME}/Downloads" $TMUX_SOURCE_DIR
 
 echo "Remove source dir"
-rm -rf $TMUX_SOURCE_DIR
+rm -rf $TMUX_SOURCE_PATH
 
-echo "Do you want to link to the tmux.conf provided?"
-read -p "Your tmux.conf will be backed up. [y/N] " -r
+# ======================
+# === tmux.conf file ===
+# ======================
+
+echo "Some of the features require specific configuration."
+echo "Your tmux.conf will be backed up."
+
+read -p "Do you want to link to the tmux.conf provided? [y/N]: " -r
 echo
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
