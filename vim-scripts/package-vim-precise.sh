447e95589a61c381aa02e2c4d59ab3bd4065afee
Luca <blmlcu@gmail.com>
2014-03-03 23:23:50 +0000

    Update the vim packaging script to actually create the package

--- vim-scripts/package-vim-precise.sh
+++ vim-scripts/package-vim-precise.sh
@@ -11,7 +11,8 @@ vim_source_dir="${HOME}/Downloads/vim-src"
 
 pull_vim_repo "${vim_repo_dir}" "${vim_source_dir}"
 
-patch_number=$(get_vim_patch_number "${vim_source_dir}")
+# These variables have to computed after pulling, otherwise the source dir would
+# not be available to get the necessary data.
 version_full=$(get_vim_version_full "${vim_source_dir}")
 vim_package_dir="${HOME}/Downloads/vim-${version_full}"
 
@@ -21,8 +22,9 @@ vim_configure_and_make "${vim_source_dir}"
 
 vim_make_install_for_package "${vim_source_dir}" "${vim_package_dir}"
 
-create_vim_control_file \
-  "${vim_source_dir}" \
-  "${vim_package_dir}" \
-  "${currdir}/vim-dependencies-precise.txt" \
-  "${currdir}/vim-deb-package-control-file" \
+create_vim_control_file "${vim_source_dir}" \
+                        "${vim_package_dir}" \
+                        "${currdir}/vim-dependencies-precise.txt" \
+                        "${currdir}/vim-deb-package-control-file" \
+
+dpkg-deb --build "${vim_package_dir}"
