b9012ef914060250f30a84bdaa1db88963dded71
Luca <blmlcu@gmail.com>
2014-03-04 08:28:37 +0000

    Add arch and OS version to the vim package name

--- vim-scripts/package-vim-precise.sh
+++ vim-scripts/package-vim-precise.sh
@@ -14,7 +14,7 @@ pull_vim_repo "${vim_repo_dir}" "${vim_source_dir}"
 # These variables have to computed after pulling, otherwise the source dir would
 # not be available to get the necessary data.
 version_full=$(get_vim_version_full "${vim_source_dir}")
-vim_package_dir="${HOME}/Downloads/vim-${version_full}"
+vim_package_dir="${HOME}/Downloads/vim-${version_full}-amd64-precise"
 
 install_dependencies "${currdir}/vim-dependencies-precise.txt"
 
